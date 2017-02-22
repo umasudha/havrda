e753511fbf9660dda05db710ce5701faeb516606
Haseenasikkandar <haseenasikkandartce@gmail.com>
2017-02-22 15:03:25 +0530

    Update info.c

--- src/info.c
+++ src/info.c
@@ -58,7 +58,7 @@ double ComputeGain(double BaseInfo, float UnknFrac, DiscrValue MaxVal,
 
     /*  Compute total info after split, by summing the
 	info of each of the subsets formed by the test  */
-    BaseInfo = TotalInfo(GEnv.Freq[v], 1, MaxClass);
+   // BaseInfo = TotalInfo(GEnv.Freq[v], 1, MaxClass);
     ForEach(v, 1, MaxVal)
     {
 	ThisInfo += TotalInfo(GEnv.Freq[v], 1, MaxClass);
@@ -87,7 +87,7 @@ double TotalInfo(double V[], DiscrValue MinVal, DiscrValue MaxVal)
     DiscrValue	v;
     double	Sum=0.0, TotalCases=0,Sum1=0.0;
     CaseCount	N;
-double alpha =2.25;
+double alpha =8.6;
 	double q= 1/(1-alpha);
     ForEach(v, MinVal, MaxVal)
     {
