1484fe9e169de62c2feaf1c75b967a7d726ae548
Haseenasikkandar <haseenasikkandartce@gmail.com>
2017-02-24 18:36:54 +0530

    Update info.c

--- src/info.c
+++ src/info.c
@@ -113,7 +113,7 @@ double alpha =8.6;
 		count[i] = -1 * count[i];
 	}
 	count[i] /= TotalCases;
-	Sum = Sum - pow(TotalCases,alpha);
+	Sum = Sum /TotalCases;
 	Sum =(Sum -1)*q;	
 	Sum *= count[i];
 	
